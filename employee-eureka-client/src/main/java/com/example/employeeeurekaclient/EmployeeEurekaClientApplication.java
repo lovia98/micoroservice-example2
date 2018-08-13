3de04262feaefc058f9ee22b07e261cc1003fa96
한주희 <lovia98@naver.com>
2018-08-13 18:57:08 +0900

    fisrt commit

---
+++ employee-eureka-client/src/main/java/com/example/employeeeurekaclient/EmployeeEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeeeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EmployeeEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeEurekaClientApplication.class, args);
+	}
+}
